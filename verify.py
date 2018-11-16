d1a589cf017730bce590727e96faac90a891cde8
HankChow <280630620@qq.com>
2018-11-16 17:51:39 +0800

    验证服务器

---
+++ verify.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import WXBizMsgCrypt
+from flask import Flask
+from flask import request
+import config
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    sToken = config.TOKEN
+    sEncodingAESKey = config.ENCODINGAESKEY
+    sCorpID = config.CORPID
+    msg_signature = request.args.get('msg_signature')
+    timestamp = request.args.get('timestamp')
+    nonce = request.args.get('nonce')
+    echostr = request.args.get('echostr')
+    wxcpt = WXBizMsgCrypt.WXBizMsgCrypt(sToken,sEncodingAESKey,sCorpID)
+    ret, sEchoStr = wxcpt.VerifyURL(msg_signature, timestamp, nonce, echostr)
+    return sEchoStr
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=config.SERVERPORT if config.SERVERPORT else 80, debug=True)
